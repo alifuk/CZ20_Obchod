5889688cfbf26832f647ac4fa41fea441f58176d
AlbertM <mysak@cloudcode.cz>
2024-09-14 11:27:11 +0200

    feat: první šablona

---
+++ viewer/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import TemplateView
+
+class MainPageView(TemplateView):
+  template_name = 'index.html'
+  extra_context = {}
